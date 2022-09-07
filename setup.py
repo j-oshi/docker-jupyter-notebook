5fa031089d80010b834fb26b9c50f8302d13e3c2
j-oshi <jtoshin@msn.com>
2022-09-07 08:07:18 +0100

    update setuptools and dockerfile

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 from setuptools import find_packages, setup
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath( os.path.dirname(__file__)))
-
-
-requirements_path="requirements.txt"
-with open(requirements_path) as requirements_file:
-    requirements = requirements_file.read().splitlines()
 
 setup(
-    name="generic_analysis",
-    version="0.0.1",
-    description = "Generic analysis",
-    long_description="Analysing with Python.",
+    name="example_notebook",
+    version="1.0.0",
+    description = "Example notebook",
+    long_description="Running notebook examples.",
     url="https://github.com/j-oshi/docker-jupyter-notebook",
-    author= "J Oshin",
-    packages= find_packages(),
-    install_requires=requirements,
+    author="J Oshin",
+    packages=find_packages(
+        where='src',
+    ),
     classifiers=[
         "Programming Language :: Python :: 3"
     ],
